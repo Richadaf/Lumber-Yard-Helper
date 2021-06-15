4e6ff07c81eadc10c75b380517cae480b7eb43c0
Richard Famoroti <richardfamoroti@gmail.com>
2021-06-15 14:21:22 +0100

    Comp Sci AP 2014-2015

---
+++ Lumber Yard Helper/Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLumberyard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.TxtThickness = New System.Windows.Forms.TextBox
+        Me.txtLength = New System.Windows.Forms.TextBox
+        Me.txtWidth = New System.Windows.Forms.TextBox
+        Me.btnCalculate = New System.Windows.Forms.Button
+        Me.lblType = New System.Windows.Forms.Label
+        Me.lblBoardF = New System.Windows.Forms.Label
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(13, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Thickness(Inches)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(13, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 16)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Length(Feet)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(12, 119)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 16)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Width(Inches)"
+        '
+        'TxtThickness
+        '
+        Me.TxtThickness.Location = New System.Drawing.Point(136, 40)
+        Me.TxtThickness.Name = "TxtThickness"
+        Me.TxtThickness.Size = New System.Drawing.Size(66, 20)
+        Me.TxtThickness.TabIndex = 1
+        '
+        'txtLength
+        '
+        Me.txtLength.Location = New System.Drawing.Point(136, 79)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(66, 20)
+        Me.txtLength.TabIndex = 1
+        '
+        'txtWidth
+        '
+        Me.txtWidth.Location = New System.Drawing.Point(136, 115)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(66, 20)
+        Me.txtWidth.TabIndex = 1
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.ForeColor = System.Drawing.Color.Black
+        Me.btnCalculate.Location = New System.Drawing.Point(208, 37)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(95, 37)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblType
+        '
+        Me.lblType.AutoSize = True
+        Me.lblType.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblType.ForeColor = System.Drawing.Color.Black
+        Me.lblType.Location = New System.Drawing.Point(219, 86)
+        Me.lblType.Name = "lblType"
+        Me.lblType.Size = New System.Drawing.Size(43, 16)
+        Me.lblType.TabIndex = 3
+        Me.lblType.Text = "Type:"
+        '
+        'lblBoardF
+        '
+        Me.lblBoardF.AutoSize = True
+        Me.lblBoardF.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBoardF.ForeColor = System.Drawing.Color.Black
+        Me.lblBoardF.Location = New System.Drawing.Point(219, 119)
+        Me.lblBoardF.Name = "lblBoardF"
+        Me.lblBoardF.Size = New System.Drawing.Size(78, 16)
+        Me.lblBoardF.TabIndex = 3
+        Me.lblBoardF.Text = "Board Feet:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(1, 181)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(119, 85)
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'frmLumberyard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Lime
+        Me.ClientSize = New System.Drawing.Size(371, 289)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblBoardF)
+        Me.Controls.Add(Me.lblType)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtWidth)
+        Me.Controls.Add(Me.txtLength)
+        Me.Controls.Add(Me.TxtThickness)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "frmLumberyard"
+        Me.Text = "LumberYard Helper Program"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TxtThickness As System.Windows.Forms.TextBox
+    Friend WithEvents txtLength As System.Windows.Forms.TextBox
+    Friend WithEvents txtWidth As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lblType As System.Windows.Forms.Label
+    Friend WithEvents lblBoardF As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Lumber Yard Helper/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class frmLumberyard
+
+    Private Sub frmLumberyard_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        lblType.Text = Nothing
+        lblBoardF.Text = Nothing
+    End Sub
+
+    Private Sub btnCalculate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalculate.Click
+        Dim sngThickness, sngLength, sngwidth As Single
+        Dim strClassification As String 'For the Return value of Function CutClassification
+        Dim sngBoardF
+        sngThickness = Val(TxtThickness.Text)
+        sngLength = Val(txtLength.Text)
+        sngwidth = Val(txtWidth.Text)
+        'Adjust Function Return data Type
+
+        lblType.Text = "Type: " & cutClassification(sngThickness, sngLength, sngwidth, strClassification)
+        lblBoardF.Text = "Board Feet: " & BoardFeet(sngThickness, sngLength, sngwidth, sngBoardF)
+
+
+    End Sub
+    Function BoardFeet(ByVal sngThickness As Single, ByVal sngLength As Single, ByVal sngwidth As Single, ByVal sngboardFeet As Single)
+        sngThickness = Val(TxtThickness.Text)
+        sngLength = Val(txtLength.Text)
+        sngwidth = Val(txtWidth.Text)
+
+        sngboardFeet = ((sngThickness * (sngLength * 12) * sngwidth) / (12 * 12 * 1))
+
+
+        Return sngboardFeet
+
+    End Function
+    Function cutClassification(ByVal sngThickness As Single, _
+                                 ByVal sngLength As Single, ByVal sngWidth As Single, ByVal strClassification As String)
+
+        sngThickness = Val(TxtThickness.Text)
+        sngLength = Val(txtLength.Text)
+        sngWidth = Val(txtWidth.Text)
+
+        If sngThickness <= 1 Then
+            strClassification = "Board"
+            PictureBox1.Image = Image.FromFile("Board.jpg")
+        ElseIf sngThickness >= 2 Or sngThickness <= 3 Then
+            strClassification = "Lumber"
+            PictureBox1.Image = Image.FromFile("Lumber.jpg")
+        ElseIf sngThickness > 4 Then
+            strClassification = "Timber"
+            PictureBox1.Image = Image.FromFile("Timber.jpg")
+        End If
+        Return strClassification
+    End Function
+
+
+
+End Class
